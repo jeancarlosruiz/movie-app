91b39d84f0b00df64dd583985aec328f5549a0a1
Jean Carlos Ruiz <66891780+jeancarlosruiz@users.noreply.github.com>
2024-02-22 22:24:38 -0400

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,75 @@
-// import { useState } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { MovieList } from './components';
+import { Input } from '@nextui-org/react';
+import data from '../data.json';
+import { uniqueID } from '../helper/uniqueId';
+
+const initialState = () => {
+  const storedMovies = localStorage.getItem('moviesList');
+
+  return storedMovies ? JSON.parse(storedMovies) : data;
+};
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [movies, setMovies] = useState(initialState);
+
+  const handleSearchTerm = useCallback(
+    (value) => {
+      setSearchTerm(value);
+    },
+    [searchTerm]
+  );
+
+  useEffect(() => {
+    localStorage.setItem('moviesList', JSON.stringify(movies));
+  }, [movies]);
+
+  const handleFilteredMovies = () => {
+    if (!searchTerm) {
+      return movies;
+    } else {
+      return movies.filter((movie) =>
+        movie.Title.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+  };
+
+  const onAddMovie = (title, description, category) => {
+    const newMovie = {
+      id: uniqueID(),
+      title,
+      description,
+      category,
+    };
+
+    const prevMovies = JSON.parse(localStorage.getItem('moviesList'));
+    const newMoviesList = [...prevMovies, newMovie];
+
+    setMovies(newMoviesList);
+  };
+
+  const onDeleteMovie = () => {
+    console.log('Eliminando Movie');
+  };
+
   return (
     <>
-      <h1>Hola mundo</h1>
-      <MovieList />
+      <h1>Movie App</h1>
+      <div className='flex w-full flex-wrap md:flex-nowrap gap-4'>
+        <Input
+          type='text'
+          label='Movie Filter'
+          placeholder='Enter your email'
+          value={searchTerm}
+          onChange={(e) => handleSearchTerm(e.target.value)}
+        />
+      </div>
+      <MovieList
+        movies={handleFilteredMovies()}
+        onAddMovie={onAddMovie}
+        onDeleteMovie={onDeleteMovie}
+      />
     </>
   );
 }
--- src/components/movieList/movieList.jsx
+++ src/components/movieList/movieList.jsx
@@ -1,13 +1,14 @@
 import { Movie } from '../index.js';
-import data from '../../../data.json';
 
 function MovieList({ movies, onAddMovie, onDeleteMovie }) {
   return (
-    <section>
-      {data.map((movie) => (
-        <Movie key={movie.Id} movie={movie} />
-      ))}
-    </section>
+    <>
+      <section>
+        {movies.map((movie) => (
+          <Movie key={movie.Id} movie={movie} />
+        ))}
+      </section>
+    </>
   );
 }
 
